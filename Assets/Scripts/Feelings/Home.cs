4312a1bc9dcb9f008253f4923a1523ee578a8e74
Gal Pasternak <galman33@gmail.com>
2019-01-25 11:37:11 +0200

    Home feelings

---
+++ Assets/Scripts/Feelings/Home.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+
+    public AudioSource HomeMusic;
+    public Transform Sun;
+
+    public float MinVolume;
+    public float MaxVolume;
+
+    public float MinSunAngle;
+    public float MaxSunAngle;
+
+    public int Levels;
+
+    private float _levelTarget;
+    private float _level;
+
+    public void SetLevel(int level)
+    {
+        _levelTarget = level;
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        _levelTarget = _level = 0;
+        UpdateLevel();
+    }
+
+    private void Update()
+    {
+        _levelTarget = Mathf.Clamp(_levelTarget, 0, Levels);
+        _level = Mathf.SmoothStep(_level, _levelTarget, Time.deltaTime * 2);
+        UpdateLevel();
+    }
+
+    private void UpdateLevel()
+    {
+        HomeMusic.volume = Mathf.Lerp(MinVolume, MaxVolume, _level / Levels);
+
+        var sunAngles = Sun.localEulerAngles;
+        sunAngles.x = Mathf.Lerp(MinSunAngle, MaxSunAngle, _level / Levels);
+        Sun.localEulerAngles = sunAngles;
+    }
+
+    public void NextHomeLevel()
+    {
+        _levelTarget++;
+    }
+}
--- Assets/Scripts/FeelingsManager.cs
+++ Assets/Scripts/FeelingsManager.cs
@@ -7,12 +7,15 @@ public class FeelingsManager : MonoBehaviour
 
     public GameObject Wet;
     public GameObject Cold;
+    public Home Home;
 
     [ContextMenu("Stop All")]
     public void StopAll()
     {
         Wet.SetActive(false);
         Cold.SetActive(false);
+        Home.gameObject.SetActive(false);
+        Home.SetLevel(0);
     }
 
     [ContextMenu("Start Wet")]
@@ -25,6 +28,18 @@ public class FeelingsManager : MonoBehaviour
     public void StartCold()
     {
         Cold.SetActive(true);
+    }
+
+    [ContextMenu("Start Home")]
+    public void StartHome()
+    {
+        Home.gameObject.SetActive(true);
+    }
+
+    [ContextMenu("Next Home")]
+    public void NextHome()
+    {
+        Home.NextHomeLevel();
     }
 
 }
