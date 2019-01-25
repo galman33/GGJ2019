f593bde65090520605599055dbb940fc744c68f8
Gal Pasternak <galman33@gmail.com>
2019-01-25 14:29:34 +0200

    Added cold to home

--- Assets/Scripts/FeelingsChanger.cs
+++ Assets/Scripts/FeelingsChanger.cs
@@ -7,11 +7,24 @@ public class FeelingsChanger : MonoBehaviour
 
     public FeelingsManager FeelingsManager;
 
+    public LayerMask WallsLayer;
+    public LayerMask PropsLayer;
+
+    public Transform BedRaysStart;
+
     public Transform BedMustRayChecks;
     public Transform[] BedRaysChecks;
-    public float BedRayCheckDistance;
     public int EnoughBedRayHits;
 
+    public float RayCheckDistance;
+
+
+    private GameObject[] _warmers;
+
+    private void Start()
+    {
+        _warmers = GameObject.FindGameObjectsWithTag("Warmer");
+    }
 
     private void Update()
     {
@@ -19,12 +32,12 @@ public class FeelingsChanger : MonoBehaviour
         {
             case FeelingsManager.Feelings.Wet:
 
-                if (!Physics.Raycast(BedMustRayChecks.position, BedMustRayChecks.forward, BedRayCheckDistance))
+                if (!Physics.Raycast(BedMustRayChecks.position, BedMustRayChecks.forward, RayCheckDistance, WallsLayer.value))
                     break;
 
                 int rayHitsCount = 0;
                 foreach (var t in BedRaysChecks)
-                    if (Physics.Raycast(t.position, t.forward, BedRayCheckDistance))
+                    if (Physics.Raycast(t.position, t.forward, RayCheckDistance, WallsLayer.value))
                         rayHitsCount++;
 
                 if (rayHitsCount >= EnoughBedRayHits)
@@ -32,6 +45,18 @@ public class FeelingsChanger : MonoBehaviour
 
                 break;
             case FeelingsManager.Feelings.Cold:
+                foreach(var warmer in _warmers)
+                {
+                    float distance = Vector3.Distance(BedRaysStart.position, warmer.transform.position);
+                    if (distance <= RayCheckDistance)
+                    {
+                        if(Physics.Raycast(BedRaysStart.position, warmer.transform.position - BedRaysStart.position, RayCheckDistance, WallsLayer.value))
+                        {
+                            FeelingsManager.StartHome();
+                            break;
+                        }
+                    }
+                }
                 break;
             case FeelingsManager.Feelings.Home:
                 break;
