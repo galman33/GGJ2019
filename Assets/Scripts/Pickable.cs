a02c1c8866b27b9f17c748b609130dec0ad1f5a4
De-Panther <depanther@gmail.com>
2019-01-25 18:06:13 +0200

    Pickaboo aaa... Pickable

---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[@RequireComponent(typeof(Rigidbody))]
+public class Pickable : MonoBehaviour, IPointerDownHandler
+{
+  private Transform cameraTransform;
+  private Rigidbody _rigidbody;
+
+  private void Awake()
+  {
+    _rigidbody = GetComponent<Rigidbody>();
+  }
+
+  private void Update()
+  {
+    if (cameraTransform != null)
+    {
+      if (Input.GetMouseButtonUp(0))
+      {
+        cameraTransform = null;
+        _rigidbody.useGravity = true;
+        _rigidbody.isKinematic = false;
+      }
+    }
+  }
+
+  private void FixedUpdate()
+  {
+    if (cameraTransform != null)
+    {
+      _rigidbody.velocity = Vector3.zero;
+      _rigidbody.angularVelocity = Vector3.zero;
+      _rigidbody.MovePosition(cameraTransform.position + (cameraTransform.forward * 2f));
+      _rigidbody.MoveRotation(cameraTransform.rotation);
+    }
+  }
+
+  public void OnPointerDown(PointerEventData eventData)
+  {
+    cameraTransform = eventData.pressEventCamera.transform;
+    _rigidbody.useGravity = false;
+    _rigidbody.isKinematic = true;
+  }
+}
