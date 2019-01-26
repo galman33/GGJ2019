e526f48762294a45d24fa89f1aff212f452df6bf
De-Panther <depanther@gmail.com>
2019-01-26 13:58:04 +0200

    StartScreen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+  public GameObject startCanvas;
+  public GameObject startText;
+  private bool gameStart = false;
+
+  void Awake()
+  {
+    startCanvas.SetActive(true);
+  }
+
+  public void StartGame()
+  {
+    if (gameStart)
+    {
+      return;
+    }
+    gameStart = true;
+    startText.SetActive(false);
+    StartCoroutine(StartGameCoroutine());
+  }
+
+  private IEnumerator StartGameCoroutine()
+  {
+    yield return new WaitForSeconds(0.5f);
+    GameStart.instance.StartGame();
+    startCanvas.SetActive(false);
+  }
+}
