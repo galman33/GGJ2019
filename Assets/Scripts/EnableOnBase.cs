f9340137482260a2de7ea4b3c06f2ec82d6c0f48
Gal Pasternak <galman33@gmail.com>
2019-01-26 15:42:29 +0200

    Added painting

--- Assets/Scripts/EnableOnBase.cs
+++ Assets/Scripts/EnableOnBase.cs
@@ -6,6 +6,7 @@ public class EnableOnBase : MonoBehaviour
 {
 
     public GameObject ToEnable;
+    public GameObject ToDisable;
 
 
     private bool _firstTime = true;
@@ -15,7 +16,8 @@ public class EnableOnBase : MonoBehaviour
         if(other.CompareTag("Base"))
         {
             ToEnable?.SetActive(true);
-            if(_firstTime)
+            ToDisable?.SetActive(false);
+            if (_firstTime)
             {
                 _firstTime = false;
                 if (gameObject.CompareTag("Happy"))
@@ -29,6 +31,7 @@ public class EnableOnBase : MonoBehaviour
         if (other.CompareTag("Base"))
         {
             ToEnable?.SetActive(false);
+            ToDisable?.SetActive(true);
         }
     }
 
